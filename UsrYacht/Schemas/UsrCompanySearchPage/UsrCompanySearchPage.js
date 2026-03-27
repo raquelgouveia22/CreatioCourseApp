c6e59d38087e732dccaab4b816e31264ec0d6f38
Raquel Gouveia <raquel.gouveia@peaceful-evolution.com>
2026-03-27 12:44:48 +0000

    Session 10: (end of session status) "Call Web Service" process item to call 3rd-party REST API

---
+++ UsrYacht/Schemas/UsrCompanySearchPage/UsrCompanySearchPage.js
@@ -0,0 +1,99 @@
+define("UsrCompanySearchPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "PageTitle",
+				"values": {
+					"caption": "#MacrosTemplateString(#ResourceString(PageTitle_caption)#)#",
+					"visible": true
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "MainContainer",
+				"values": {
+					"alignItems": "stretch"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "CompanyName",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_3mdkapd",
+					"control": "$PageParameters_UsrTextParameter1_3mdkapd",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": false,
+					"multiline": false,
+					"labelPosition": "above"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "CountryCode",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_tc1j1yi",
+					"control": "$PageParameters_UsrTextParameter1_tc1j1yi",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": false,
+					"multiline": false,
+					"labelPosition": "above"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrTextParameter1_3mdkapd": {
+						"modelConfig": {
+							"path": "PageParameters.UsrCompanyName"
+						}
+					},
+					"PageParameters_UsrTextParameter1_tc1j1yi": {
+						"modelConfig": {
+							"path": "PageParameters.UsrCountryCode"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
